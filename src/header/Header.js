fa351c313c4405ac7f0b51c36fae0e36761a9cca
Elliot Plant <elliotaplant@gmail.com>
2020-07-07 10:17:48 -0700

    Just a left border

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,6 +4,9 @@ import './Header.css'
 export default function Header() {
   return <header className="Header-header">
     <h1 className="Header-title">Etherstream</h1>
-    <div className="Header-paragraph">Stream event logs from any ethereum contract</div>
+    <div className="Header-paragraph">
+      Stream ethereum event logs.
+      Made by <a href="http://elliotplant.com">Elliot Plant</a>.
+    </div>
   </header>
 }
